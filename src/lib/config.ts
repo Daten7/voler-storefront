00f905094f79e02776015f5f55cdbe0bf87fe8d5
Daten7 <dugbarteysamuel1@gmail.com>
2024-12-27 12:49:23 +0000

    more changes same file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000/app"
+let MEDUSA_BACKEND_URL = "https://voler-9r45dju4h-samuel-kwasi-dugbarteys-projects.vercel.app/"
 
 if (process.env.MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
