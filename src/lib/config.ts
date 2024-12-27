427bca8ebfe7f686ea90cec9324598443bffa7eb
Daten7 <dugbarteysamuel1@gmail.com>
2024-12-27 12:43:49 +0000

    Another update to the same file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,14 +1,13 @@
 import Medusa from "@medusajs/js-sdk"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "http://localhost:9000/app"
 
 if (process.env.MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
 }
 
 console.log("MEDUSA_BACKEND_URL:", MEDUSA_BACKEND_URL); // Add this line
-
 
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
